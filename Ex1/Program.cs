24c8411c13fad7726723adfe3318eae02101239a
JustCheka <cheka201@yandxe.ru>
2023-08-25 13:03:12 +0300

    Ex 1

---
+++ Ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine());
+
+Random rand = new Random();
+double[,] arr = new double[m,n];
+
+for (int i = 0; i < m; i ++){
+    for (int j = 0; j < n; j++){
+        arr[i,j] = Math.Round(rand.NextDouble() * 20, 3);
+        Console.Write(arr[i,j] + " ");
+    }
+    Console.WriteLine();
+}
